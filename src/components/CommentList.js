fa7027e635f758844dd1bb2a61f36560dfb83c7f
Murillo de Morais <murillo.morais@vianuvem.com.br>
2017-11-18 17:12:28 -0200

    feat: sorted comments by voteScore

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Comment from './Comment'
 
+import sortBy from 'sort-by'
+
 const CommentList = ({ comments }) => {
+
+    const sortedComments = comments.sort(sortBy('-voteScore'))
+
     return (
         <div className="comment-list">
             <h2>Comments ({comments.length})</h2>
-            {comments.map(comment => (
+            {sortedComments.map(comment => (
                 <Comment key={comment.id} comment={comment} />
             ))}
         </div>
