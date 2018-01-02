046158ca6664a0fcdbd7bbb23902da6e42753487
Murillo de Morais <mm@in3d.com.br>
2018-01-02 19:31:45 -0200

    fix: corrected match problem

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -17,7 +17,7 @@ class PostPage extends Component{
     componentDidMount () {
         const { match, fetchPost, fetchComments, fetchCategories } = this.props
         
-        if(match.params.categoryPath === 'category') return false
+        if(match.params.categoryPath === 'category' || match.params.categoryPath === 'create') return false
         
         fetchCategories()
 
@@ -34,7 +34,7 @@ class PostPage extends Component{
         const isEditPage = match.url.split('/').includes('edit') || match.path === '/create/post'
         const readOnly = !isEditPage
 
-        if(match.params.categoryPath === 'category') return false
+        if(match.params.categoryPath === 'category' || match.params.categoryPath === 'create') return false
     
         return (
             <div className="post-page">
